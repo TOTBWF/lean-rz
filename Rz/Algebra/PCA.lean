b0f0f95631f614e97e404df8249ea22cb84d15ad
Reed Mullanix <reedmullanix@gmail.com>
2024-05-27 17:57:43 +0200

    chore: clean up

--- Rz/Algebra/PCA.lean
+++ Rz/Algebra/PCA.lean
@@ -43,58 +43,20 @@ class PAS (α : Type u) extends HasEval α where
   /-- Looking up the 0th variable resolves to the final element of the environment. -/
   var_zero_eval : (ρ : Bwd α) → (a : α) → (ρ :# a) ⊢ `(0) ⇓ a
   /-- Resolving a successor variable requires us to look up in the tail of the environment. -/
-  var_succ_eval : (ρ : Bwd α) → (a : α) → (n : Nat) → (ρ :# a) ⊢ `(n+1) ≃ ρ ⊢ `(n)
+  var_succ_eval_le : (ρ : Bwd α) → (a : α) → (n : Nat) → (ρ :# a) ⊢ `(n+1) ≤ ρ ⊢ `(n)
+  var_succ_eval_ge : (ρ : Bwd α) → (a : α) → (n : Nat) → ρ ⊢ `(n) ≤ (ρ :# a) ⊢ `(n+1)
   /-- Resolving an out-of-bounds variable diverges. -/
   var_zero_diverge : (n : Nat) → (Bwd.nil : Bwd α) ⊢ `(n) ↑
   /-- Constants reduce to themselves. -/
   const_eval : (ρ : Bwd α) → (a : α) → ρ ⊢ a ⇓ a
   /-- Applications are evaluated by evaluating both sides.
       Note the change of environment; this corresponds to the fact that substitutions do not act on values. -/
-  ap_eval : {ρ₁ ρ₂ : Bwd α} → {e₁ e₂ : FreeMagma α} → {a₁ a₂ : α} → ρ₁ ⊢ e₁ ⇓ a₁ → ρ₁ ⊢ e₂ ⇓ a₂ → ρ₁ ⊢ e₁ e₂ ≃ ρ₂ ⊢ a₁ a₂
+  ap_eval_le : {ρ₁ ρ₂ : Bwd α} → {e₁ e₂ : FreeMagma α} → {a₁ a₂ : α} → ρ₁ ⊢ e₁ ⇓ a₁ → ρ₁ ⊢ e₂ ⇓ a₂ → ρ₁ ⊢ e₁ e₂ ≤ ρ₂ ⊢ a₁ a₂
+  ap_eval_ge : {ρ₁ ρ₂ : Bwd α} → {e₁ e₂ : FreeMagma α} → {a₁ a₂ : α} → ρ₁ ⊢ e₁ ⇓ a₁ → ρ₁ ⊢ e₂ ⇓ a₂ → ρ₂ ⊢ a₁ a₂ ≤ ρ₁ ⊢ e₁ e₂
   /-- If an application is defined, then so is the left argument. -/
   ap_left_defined : {ρ : Bwd α} → {e₁ e₂ : FreeMagma α} → ρ ⊢ e₁ e₂ ↓ → ρ ⊢ e₁ ↓
   /-- If an application is defined, then so is the right argument. -/
   ap_right_defined : {ρ : Bwd α} → {e₁ e₂ : FreeMagma α} → ρ ⊢ e₁ e₂ ↓ → ρ ⊢ e₂ ↓
-
-/-- Partial Applicative Structures. -/
-class PAS' (α : Type u) where
-  ap : α → α → α → Prop
-  /-- The evaluation relation is functional. -/
-  ap_functional : {a₁ a₂ v₁ v₂ : α} → ap a₁ a₂ v₁ → ap a₁ a₂ v₂ → v₁ = v₂
-
-def PAS'.Eval [PAS' α] : (ρ : Bwd α) → FreeMagma α → α → Prop
-  | ρ, .var i, v => ∃ h, ρ.get ⟨i, h⟩ = v
-  | _, .const a, v => a = v
-  | ρ, .ap e₁ e₂, v => ∃ v₁ v₂, Eval ρ e₁ v₁ ∧ Eval ρ e₂ v₂ ∧ PAS'.ap v₁ v₂ v
-
-instance [PAS' α] : HasEval α := ⟨PAS'.Eval⟩
-
-theorem PAS'.eval_functional [PAS' α] {ρ e} {a a' : α}
-    (h1 : Eval ρ e a) (h2 : Eval ρ e a') : a = a' := by
-  induction e generalizing a a' with simp [HasEval.Eval, PAS'.Eval] at h1 h2
-  | var => obtain ⟨_, rfl⟩ := h1; obtain ⟨_, rfl⟩ := h2; rfl
-  | const => exact h1 ▸ h2
-  | ap _ _ ih1 ih2 =>
-    obtain ⟨_, ev11, _, ev12, h1⟩ := h1
-    obtain ⟨_, ev21, _, ev22, h2⟩ := h2
-    cases ih1 ev11 ev21
-    cases ih2 ev12 ev22
-    exact PAS'.ap_functional h1 h2
-
-instance [PAS' α] : PAS α where
-  eval_functional := PAS'.eval_functional
-  var_zero_eval _ _ := ⟨Nat.succ_pos _, rfl⟩
-  var_succ_eval _ _ _ :=
-    ⟨fun _ ⟨h, eq⟩ => ⟨Nat.lt_of_succ_lt_succ h, eq⟩, fun _ ⟨h, eq⟩ => ⟨Nat.succ_lt_succ h, eq⟩⟩
-  var_zero_diverge := nofun
-  const_eval _ _ := rfl
-  ap_eval h1 h2 := by
-    refine ⟨fun v' ⟨a₁', a₂', e1, e2, H⟩ => ⟨_, _, rfl, rfl, ?_⟩, fun v' ⟨_, _, rfl, rfl, H⟩ => ⟨_, _, h1, h2, H⟩⟩
-    cases PAS'.eval_functional h1 e1
-    cases PAS'.eval_functional h2 e2
-    exact H
-  ap_left_defined := fun ⟨_, _, _, h, _⟩ => ⟨_, h⟩
-  ap_right_defined := fun ⟨_, _, _, _, h, _⟩ => ⟨_, h⟩
 
 namespace PAS
 
@@ -103,14 +65,24 @@ namespace PAS
 -/
 
 /-- Variant of `var_succ_eval` that uses `Nat.pred`. -/
-theorem var_pred_eval_ne_zero
+theorem var_pred_eval_ne_zero_le
   [PAS α]
   (ρ : Bwd α) (a : α)
   (n : Nat) (h : n ≠ 0)
-  : (ρ :# a) ⊢ `(n) ≃ ρ ⊢ `(n.pred) := by
+  : (ρ :# a) ⊢ `(n) ≤ ρ ⊢ `(n.pred) := by
   match n with
   | 0 => contradiction
-  | n+1 => apply var_succ_eval
+  | n+1 => apply var_succ_eval_le
+
+theorem var_pred_eval_ne_zero_ge
+  [PAS α]
+  (ρ : Bwd α) (a : α)
+  (n : Nat) (h : n ≠ 0)
+  : ρ ⊢ `(n.pred) ≤ (ρ :# a) ⊢ `(n) := by
+  match n with
+  | 0 => contradiction
+  | n+1 => apply var_succ_eval_ge
+
 
 /-- Variable lookups evaluate to `Bwd.get` if they are in bounds. -/
 theorem var_get_eval
@@ -126,7 +98,7 @@ theorem var_get_eval
       simp [h]
       apply PAS.var_zero_eval
     else
-      apply (PAS.var_pred_eval_ne_zero ρ a x (Fin.val_ne_of_ne h)).2
+      apply (PAS.var_pred_eval_ne_zero_ge ρ a x (Fin.val_ne_of_ne h))
       rw [Bwd.get_snoc_pred_ne_zero ρ a x h]
       apply PAS.var_get_eval (x := x.pred h)
 
@@ -137,7 +109,7 @@ theorem var_of_converge [PAS α] (ρ : Bwd α) (x : Nat) : ρ ⊢ `(x) ↓ → x
   | snoc ρ a IH =>
     match x with
     | 0 => simp
-    | x + 1 => exact Nat.succ_lt_succ <| IH _ <| (var_succ_eval _ _ _).1 _ H
+    | x + 1 => exact Nat.succ_lt_succ <| IH _ <| var_succ_eval_le _ _ _ _ H
 
 /-!
 ## Evaluation Lemmas
@@ -150,6 +122,7 @@ theorem const_eval_eq
   (a a' : α) (a_eval : ρ ⊢ a ⇓ a')
   : a = a' :=
     eval_functional (const_eval ρ a) a_eval
+
 
 /-- Evaluation of constants is invariant under environments. -/
 theorem const_eval_stable
@@ -185,8 +158,8 @@ theorem closed_eval_stable
         apply closed_eval_stable
         · exact e₂_closed
         · exact a₂_eval_ρ₁
-      apply (ap_eval a₁_eval_ρ₂ a₂_eval_ρ₂).2
-      apply (ap_eval (ρ₂ := ρ₂) a₁_eval_ρ₁ a₂_eval_ρ₁).1
+      apply ap_eval_ge a₁_eval_ρ₂ a₂_eval_ρ₂
+      apply ap_eval_le (ρ₂ := ρ₂) a₁_eval_ρ₁ a₂_eval_ρ₁
       · exact a_eval
 
 /-!
@@ -205,31 +178,67 @@ theorem ap_defined
   (v_def : ρ ⊢ a₁ a₂ ↓)
   : ρ ⊢ e₁ e₂ ↓ :=
     let ⟨ w , h ⟩ := v_def
-    ⟨ w , (ap_eval e₁_def e₂_def).2 w h ⟩
+    ⟨ w , ap_eval_ge e₁_def e₂_def w h ⟩
 
-def toPAS' (α) [PAS α] : PAS' α where
-  ap a₁ a₂ v := .nil ⊢ a₁ a₂ ⇓ v
-  ap_functional := eval_functional
+def ap_congl
+  [PAS α]
+  {ρ : Bwd α} {e₁ e₂ : FreeMagma α} {v₁ v : α}
+  (eval : ρ ⊢ e₁ e₂ ⇓ v)
+  (e₁_eval : ρ ⊢ e₁ ⇓ v₁)
+  : ρ ⊢ v₁ e₂ ⇓ v := by
+  have ⟨ v₂ , e₂_eval ⟩ := ap_right_defined ⟨ v , eval ⟩
+  apply ap_eval_ge (a₁ := v₁) (a₂ := v₂) _ _
+  · apply ap_eval_le (ρ₂ := ρ) _ _
+    · exact eval
+    · exact e₁_eval
+    · exact e₂_eval
+  · apply const_eval
+  · exact e₂_eval
 
-theorem toPAS'_toPAS [inst : PAS α] : @instPASOfPAS' _ (toPAS' α) = inst := by
-  suffices (toPAS' α).Eval = HasEval.Eval by
-    let {..} := inst; unfold instPASOfPAS' instHasEvalOfPAS'; congr
-  ext ρ e v; constructor <;> intro H
-  · induction e generalizing v with simp [PAS'.Eval] at H
-    | const => cases H; apply const_eval
-    | var => obtain ⟨_, rfl⟩ := H; exact var_get_eval ρ ⟨_, _⟩
-    | ap _ _ ih1 ih2 =>
-      obtain ⟨_, h1, _, h2, h3⟩ := H
-      exact (ap_eval (ih1 _ h1) (ih2 _ h2)).2 _ h3
-  · induction e generalizing v with simp [PAS'.Eval]
-    | const => exact eval_functional (const_eval ..) H
-    | var =>
-      have h := var_of_converge _ _ ⟨_, H⟩
-      exact ⟨h, eval_functional (var_get_eval _ ⟨_, h⟩) H⟩
-    | ap _ _ ih1 ih2 =>
-      have ⟨a₁, h1⟩ := ap_left_defined ⟨_, H⟩
-      have ⟨a₂, h2⟩ := ap_right_defined ⟨_, H⟩
-      exact ⟨_, ih1 _ h1, _, ih2 _ h2, (ap_eval h1 h2).1 _ H⟩
+def ap_congl_inv
+  [PAS α]
+  {ρ : Bwd α} {e₁ e₂ : FreeMagma α} {v₁ v : α}
+  (eval : ρ ⊢ v₁ e₂ ⇓ v)
+  (e₁_eval : ρ ⊢ e₁ ⇓ v₁)
+  : ρ ⊢ e₁ e₂ ⇓ v := by
+  have ⟨ v₂ , e₂_eval ⟩ := ap_right_defined ⟨ v , eval ⟩
+  apply ap_eval_ge (ρ₂ := ρ) (a₁ := v₁) (a₂ := v₂) _ _
+  · apply ap_eval_le (ρ₂ := ρ) _ _
+    · exact eval
+    · apply const_eval
+    · exact e₂_eval
+  · exact e₁_eval
+  · exact e₂_eval
+
+def ap_congr
+  [PAS α]
+  {ρ : Bwd α} {e₁ e₂ : FreeMagma α} {v₂ v : α}
+  (eval : ρ ⊢ e₁ e₂ ⇓ v)
+  (e₂_eval : ρ ⊢ e₂ ⇓ v₂)
+  : ρ ⊢ e₁ v₂ ⇓ v := by
+  have ⟨ v₁ , e₁_eval ⟩ := ap_left_defined ⟨ v , eval ⟩
+  apply ap_eval_ge (a₁ := v₁) (a₂ := v₂) _ _
+  · apply ap_eval_le (ρ₂ := ρ) _ _
+    · exact eval
+    · exact e₁_eval
+    · exact e₂_eval
+  · exact e₁_eval
+  · apply const_eval
+
+def ap_congr_inv
+  [PAS α]
+  {ρ : Bwd α} {e₁ e₂ : FreeMagma α} {v₂ v : α}
+  (eval : ρ ⊢ e₁ v₂ ⇓ v)
+  (e₂_eval : ρ ⊢ e₂ ⇓ v₂)
+  : ρ ⊢ e₁ e₂ ⇓ v := by
+  have ⟨ v₁ , e₁_eval ⟩ := ap_left_defined ⟨ v , eval ⟩
+  apply ap_eval_ge (ρ₂ := ρ) (a₁ := v₁) (a₂ := v₂) _ _
+  · apply ap_eval_le (ρ₂ := ρ) _ _
+    · exact eval
+    · exact e₁_eval
+    · apply const_eval
+  · exact e₁_eval
+  · exact e₂_eval
 
 end PAS
 
@@ -240,9 +249,48 @@ class PCA (α : Type u) extends PAS α where
   /-- Bracket abstraction yields a value. -/
   abs_defined : (ρ : Bwd α) → (e : FreeMagma α) → ρ ⊢ $(abs e) ↓
   /-- Bracket abstraction has a β-law. -/
-  abs_eval : (ρ : Bwd α) → (a : α) → (e : FreeMagma α) → ρ ⊢ $(abs e) a ≃ (ρ :# a) ⊢ e
+  abs_eval_le : (ρ : Bwd α) → (a : α) → (e : FreeMagma α) → ρ ⊢ $(abs e) a ≤ (ρ :# a) ⊢ e
+  abs_eval_ge : (ρ : Bwd α) → (a : α) → (e : FreeMagma α) → (ρ :# a) ⊢ e ≤ ρ ⊢ $(abs e) a
 
 namespace PCA
+
+protected def i [PCA α] : FreeMagma α :=
+  abs (.var 0)
+
+protected def i_eval_le [PCA α] (ρ : Bwd α) (e : FreeMagma α) : ρ ⊢ PCA.i e ≤ ρ ⊢ e := by
+  intros v v_eval
+  have ⟨ v' , v'_eval ⟩ := PAS.ap_right_defined ⟨ v , v_eval ⟩
+  have : ρ ⊢ PCA.i v' ⇓ v := PAS.ap_congr v_eval v'_eval
+  have l : (ρ :# v') ⊢ `(0) ⇓ v := abs_eval_le ρ v' (.var 0) v this
+  have r : (ρ :# v') ⊢ `(0) ⇓ v' := by
+   apply PAS.var_zero_eval
+  have : v = v' := PAS.eval_functional l r
+  rwa [this]
+
+protected def i_eval_ge [PCA α] (ρ : Bwd α) (e : FreeMagma α) : ρ ⊢ e ≤ ρ ⊢ PCA.i e := by
+  intros v v_eval
+  have : ρ ⊢ PCA.i v ⇓ v := by
+    apply abs_eval_ge ρ v (.var 0)
+    apply PAS.var_zero_eval
+  exact (PAS.ap_congr_inv this v_eval)
+
+protected def i_eval_eq
+  [PCA α] {ρ : Bwd α} {v v' : α}
+  (v_eval : ρ ⊢ PCA.i v ⇓ v')
+  : v = v' := by
+    apply PAS.eval_functional (ρ := ρ)
+    · apply PCA.i_eval_ge
+      · apply PAS.const_eval
+    · exact v_eval
+
+protected def c [PCA α] (e₁ e₂ : FreeMagma α) : FreeMagma α :=
+  abs («magma» e₂ (e₁ `(0)))
+
+-- protected def c_eval_le
+--   [PCA α]
+--   {ρ : Bwd α}
+--   {e₁ e₂ : FreeMagma α} {v₁ v₂ : α}
+--   (e₁_eval : ρ ⊢ e₁ ⇓ v₁) (e₂)
 
 protected def true [PCA α] : FreeMagma α :=
   (abs (abs (.var 1)))
@@ -266,9 +314,12 @@ class SKI (α : Type u) extends PAS α where
   i : α
   s_defined_2 : {ρ : Bwd α} → {e₁ e₂ : FreeMagma α} → ρ ⊢ e₁ ↓ → ρ ⊢ e₂ ↓ → ρ ⊢ s e₁ e₂ ↓
   k_defined : {ρ : Bwd α} → {e : FreeMagma α} → ρ ⊢ e ↓ → ρ ⊢ k e ↓
-  s_eval : (ρ : Bwd α) → (e₁ e₂ e₃ : FreeMagma α) → ρ ⊢ s e₁ e₂ e₃ ≃ ρ ⊢ (e₁ e₃) (e₂ e₃)
-  k_eval : (ρ : Bwd α) → (e₁ e₂ : FreeMagma α) → ρ ⊢ e₂ ↓ → ρ ⊢ k e₁ e₂ ≃ ρ ⊢ e₁
-  i_eval : (ρ : Bwd α) → (e : FreeMagma α) → ρ ⊢ i e ≃ ρ ⊢ e
+  s_eval_le : (ρ : Bwd α) → (e₁ e₂ e₃ : FreeMagma α) → ρ ⊢ s e₁ e₂ e₃ ≤ ρ ⊢ (e₁ e₃) (e₂ e₃)
+  s_eval_ge : (ρ : Bwd α) → (e₁ e₂ e₃ : FreeMagma α) → ρ ⊢ (e₁ e₃) (e₂ e₃) ≤ ρ ⊢ s e₁ e₂ e₃
+  k_eval_le : (ρ : Bwd α) → (e₁ e₂ : FreeMagma α) → ρ ⊢ e₂ ↓ → ρ ⊢ k e₁ e₂ ≤ ρ ⊢ e₁
+  k_eval_ge : (ρ : Bwd α) → (e₁ e₂ : FreeMagma α) → ρ ⊢ e₂ ↓ → ρ ⊢ e₁ ≤ ρ ⊢ k e₁ e₂
+  i_eval_le : (ρ : Bwd α) → (e : FreeMagma α) → ρ ⊢ i e ≤ ρ ⊢ e
+  i_eval_ge : (ρ : Bwd α) → (e : FreeMagma α) → ρ ⊢ e ≤ ρ ⊢ i e
 
 namespace SKI
 
@@ -318,30 +369,29 @@ def abs_refines_le [SKI α] (ρ : Bwd α) (a : α) (e : FreeMagma α) : ρ ⊢ $
   | .var 0 =>
     simp at *
     have var_eval : (ρ :# a) ⊢ `(0) ⇓ a := PAS.var_zero_eval ρ a
-    have a_eval : ρ ⊢ a ⇓ v := (i_eval ρ (FreeMagma.const a)).1 v v_eval
+    have a_eval : ρ ⊢ a ⇓ v := i_eval_le _ _ v v_eval
     rwa [←PAS.const_eval_eq ρ a v a_eval]
   | .var (n+1) =>
      simp at *
-     apply (PAS.var_succ_eval ρ a n).2
-     apply (SKI.k_eval ρ (.var n) (.const a) _).1
+     apply PAS.var_succ_eval_ge ρ a n
+     apply SKI.k_eval_le ρ (.var n) (.const a) _
      · exact v_eval
      · apply PAS.const_defined
   | .const b =>
     simp at *
     apply PAS.const_eval_stable
-    apply (SKI.k_eval ρ (.const b) (.const a) _).1
+    apply SKI.k_eval_le ρ (.const b) (.const a) _
     · exact v_eval
     · apply PAS.const_defined ρ a
   | .ap e₁ e₂ =>
     simp at *
     have ap_abs_eval : ρ ⊢ ($(abs e₁) a) ($(abs e₂) a) ⇓ v := by
-      apply (SKI.s_eval ρ (abs e₁) (abs e₂) (.const a)).1
+      apply SKI.s_eval_le ρ (abs e₁) (abs e₂) (.const a)
       exact v_eval
     have ⟨ v₁ , v₁_eval ⟩ := PAS.ap_left_defined ⟨ v , ap_abs_eval ⟩
     have ⟨ v₂ , v₂_eval ⟩ := PAS.ap_right_defined ⟨ v , ap_abs_eval ⟩
-
-    apply (PAS.ap_eval (ρ₂ := ρ) (abs_refines_le ρ a e₁ v₁ v₁_eval) (abs_refines_le ρ a e₂ v₂ v₂_eval)).2
-    apply (PAS.ap_eval v₁_eval v₂_eval).1
+    apply PAS.ap_eval_ge (ρ₂ := ρ) (abs_refines_le ρ a e₁ v₁ v₁_eval) (abs_refines_le ρ a e₂ v₂ v₂_eval)
+    apply PAS.ap_eval_le v₁_eval v₂_eval
     exact ap_abs_eval
 
 def abs_refines_ge [SKI α] (ρ : Bwd α) (a : α) (e : FreeMagma α) : (ρ :# a) ⊢ e ≤ ρ ⊢ $(abs e) a := by
@@ -350,18 +400,18 @@ def abs_refines_ge [SKI α] (ρ : Bwd α) (a : α) (e : FreeMagma α) : (ρ :# a
   | .var 0 =>
     simp at *
     have var_eval : (ρ :# a) ⊢ `(0) ⇓ a := PAS.var_zero_eval ρ a
-    apply (i_eval _ _).2
+    apply i_eval_ge _ _
     rw [PAS.eval_functional v_eval var_eval]
     apply PAS.const_eval
   | .var (n+1) =>
     simp at *
-    apply (k_eval ρ (.var n) (.const a) _).2
-    · apply (PAS.var_succ_eval ρ a n).1
+    apply k_eval_ge ρ (.var n) (.const a) _
+    · apply PAS.var_succ_eval_le ρ a n
       exact v_eval
     · apply PAS.const_defined
   | .const b =>
     simp at *
-    apply (k_eval ρ (.const b) (.const a) _).2
+    apply k_eval_ge ρ (.const b) (.const a) _
     · apply PAS.const_eval_stable
       exact v_eval
     · apply PAS.const_defined
@@ -369,9 +419,9 @@ def abs_refines_ge [SKI α] (ρ : Bwd α) (a : α) (e : FreeMagma α) : (ρ :# a
     simp at *
     have ⟨ v₁ , v₁_eval ⟩ := PAS.ap_left_defined ⟨ v , v_eval ⟩
     have ⟨ v₂ , v₂_eval ⟩ := PAS.ap_right_defined ⟨ v , v_eval ⟩
-    apply (s_eval ρ (abs e₁) (abs e₂) (.const a)).2
-    apply (PAS.ap_eval (ρ₂ := ρ) (abs_refines_ge ρ a e₁ v₁ v₁_eval) (abs_refines_ge ρ a e₂ v₂ v₂_eval)).2
-    apply (PAS.ap_eval v₁_eval v₂_eval).1
+    apply s_eval_ge ρ (abs e₁) (abs e₂) (.const a)
+    apply PAS.ap_eval_ge (ρ₂ := ρ) (abs_refines_ge ρ a e₁ v₁ v₁_eval) (abs_refines_ge ρ a e₂ v₂ v₂_eval)
+    apply PAS.ap_eval_le v₁_eval v₂_eval
     exact v_eval
 
 end SKI
